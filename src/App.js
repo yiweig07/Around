2b94b794349912383de41b04e35784a1edb85a39
Yiwei Gao <ivygao07@gmail.com>
2020-01-20 20:03:14 -0800

    top bar finished

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Topbar } from './Topbar';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <span className="App-title">Around</span>
-      </header>
+      <Topbar/>
     </div>
   );
 }
---
+++ src/Topbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+
+export function Topbar() {
+    return (
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <span className="App-title">Around</span>
+        </header>
+    );
+}
+
+
+

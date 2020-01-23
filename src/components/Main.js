3777b24a0a2409ce011c9f5c8e0217d18b62ef01
Yiwei Gao <ivygao07@gmail.com>
2020-01-22 19:26:07 -0800

    Login

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Register } from './Register';
+import { Login } from './Login';
 import '../styles/Main.css'
 
 export class Main extends React.Component {
     render() {
         return (
             <div className="main">
-                <Register/>
+                <Login />
             </div>
         );
     }

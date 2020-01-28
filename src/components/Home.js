e3fbe58c17e89983932631f3dac108eed9080d1a
Yiwei Gao <ivygao07@gmail.com>
2020-01-27 20:07:31 -0800

    Add geolocation and save position to localstorage to Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,57 @@
 import React from "react";
 import "../styles/Home.css"
 import { Tabs, Button } from 'antd';
+import { GEOLOCATION_OPTIONS, POSITION_KEY } from '../constants';
 
 const { TabPane } = Tabs;
 
 export class Home extends React.Component {
+    state = {
+      loadingGeolocation: false,
+      errorMessage: null,
+    };
+
+    getGeolocation() {
+        this.setState({
+            loadingGeolocation: true,
+            errorMessage: null,
+        });
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                this.onGeolocationSuccess,
+                this.onGeolocationFailure,
+                GEOLOCATION_OPTIONS,
+            );
+        } else {
+            this.setState({
+                loadingGeolocation: false,
+                errorMessage: 'Your browser does not support geolocation',
+            });
+        }
+    }
+
+    onGeolocationSuccess = (position) => {
+        this.setState({
+            loadingGeolocation: false,
+            errorMessage: null,
+        });
+        console.log(position);
+        const { latitude, longitude } = position.coords;
+        localStorage.setItem(POSITION_KEY, JSON.stringify({ latitude, longitude }));
+    }
+
+    onGeolocationFailure = () => {
+        this.setState({
+            loadingGeolocation: false,
+            errorMessage: 'Failed to load your position',
+        })
+    }
+
+    componentDidMount() {
+        this.getGeolocation();
+    }
+
+
     render() {
         const operations = <Button>Create New Post</Button>;
         return (
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,9 @@
 
 export const API_ROOT = 'https://around-179500.appspot.com';
 export const TOKEN_KEY = 'TOKEN';
+export const POSITION_KEY = 'POSITION';
+export const GEOLOCATION_OPTIONS = {
+    enableHighAccuracy: true,
+    maximumAge: 3600000,
+    timeout: 27000,
+};

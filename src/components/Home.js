afa4bd5dad76279cf2b68528b5850c16373d270c
Yiwei Gao <ivygao07@gmail.com>
2020-01-27 19:20:06 -0800

    Add tabs to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,24 @@
 import React from "react";
 import "../styles/Home.css"
+import { Tabs, Button } from 'antd';
 
-export function Home() {
-    return (<div>Home</div>);
+const { TabPane } = Tabs;
+
+export class Home extends React.Component {
+    render() {
+        const operations = <Button>Create New Post</Button>;
+        return (
+            <Tabs tabBarExtraContent={operations} className="main-tabs">
+                <TabPane tab="Image Posts" key="1">
+                    Content of tab 1
+                </TabPane>
+                <TabPane tab="Tab 2" key="2">
+                    Content of tab 2
+                </TabPane>
+                <TabPane tab="Tab 3" key="3">
+                    Content of tab 3
+                </TabPane>
+            </Tabs>
+        );
+    }
 }
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
 
-export const API_ROOT = 'https://around-dot-around-179500.appspot.com/api/v1';
+export const API_ROOT = 'https://around-179500.appspot.com';
 export const TOKEN_KEY = 'TOKEN';

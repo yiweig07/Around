b89cad21904b6a12462c83e2a937df40ccfa9124
Yiwei Gao <ivygao07@gmail.com>
2020-02-05 19:46:54 -0800

    video infowindow, blue marker

--- src/components/AroundMarker.js
+++ src/components/AroundMarker.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Marker, InfoWindow } from "react-google-maps";
 import "../styles/AroundMarker.css";
+import {POST_TYPE_IMAGE, POST_TYPE_VIDEO} from "../constants"
+import blueMarker from '../assets/blue-marker.svg';
 
 export class AroundMarker extends React.Component {
     state = {
@@ -12,9 +14,9 @@ export class AroundMarker extends React.Component {
         }));
     }
     render() {
-        const { location, user, message, url } = this.props.post;
-        console.log(this.props.post);
-        return (
+        const { location, user, message, url, type } = this.props.post;
+        if (type === POST_TYPE_IMAGE) {
+            return (
                 <Marker
                     position={{lat: location.lat, lng:location.lon,}}
                     onMouseOver={this.onToggleOpen}
@@ -33,6 +35,31 @@ export class AroundMarker extends React.Component {
                             null
                     )}
                 </Marker>
-        );
+            );
+        } else if (type === POST_TYPE_VIDEO) {
+            return (
+                <Marker
+                    position={{lat: location.lat, lng:location.lon,}}
+                    onClick={this.onToggleOpen}
+                    icon={{
+                        url: blueMarker,
+                        scaledSize: new window.google.maps.Size(26, 41),
+                    }}
+                >
+                    {(
+                        this.state.isOpen ?
+                            (
+                                <InfoWindow onCloseClick={this.onToggleOpen}>
+                                    <div>
+                                        <video src={url} controls={true} className="around-marker-video" />
+                                        <div>{`${user}: ${message}`}</div>
+                                    </div>
+                                </InfoWindow>
+                            ) :
+                            null
+                    )}
+                </Marker>
+            );
+        } else return null;
     }
 }
